a25e9364e4510bdadb95e7177c4ed8e78d763299
coreytegeler <coreytegeler@gmail.com>
2018-07-19 22:35:18 -0400

    grid

--- assets/coffee/scripts.coffee
+++ assets/coffee/scripts.coffee
@@ -8,3 +8,18 @@ $ ->
 	# 	$(this).marquee('pause')
 	# .on 'mouseleave', (e) ->
 	# 	$(this).marquee('resume')
+
+	$('.grid').packery
+		itemSelector: 'figure',
+		layoutMode: 'fitRows',
+		percentPosition: true,
+		# gutter: 10,
+		transitionDuration: 0
+
+	$('.grid').find('figure').each (i, figure) ->
+  	draggie = new Draggabilly( figure )
+  	grid = $(this).parents('.grid')
+	  grid.packery( 'bindDraggabillyEvents', draggie )
+
+	 $('main').on 'click', () ->
+	 		$(this).find('section').toggleClass('hidden')
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,23 @@
 $(function() {
-  return $('.marquee').marquee({
+  $('.marquee').marquee({
     gap: 0,
     delayBeforeStart: 0,
     startVisible: true,
     duplicated: true
+  });
+  $('.grid').packery({
+    itemSelector: 'figure',
+    layoutMode: 'fitRows',
+    percentPosition: true,
+    transitionDuration: 0
+  });
+  $('.grid').find('figure').each(function(i, figure) {
+    var draggie, grid;
+    draggie = new Draggabilly(figure);
+    grid = $(this).parents('.grid');
+    return grid.packery('bindDraggabillyEvents', draggie);
+  });
+  return $('main').on('click', function() {
+    return $(this).find('section').toggleClass('hidden');
   });
 });
